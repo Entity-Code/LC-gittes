9543f2fea9b0572ccca469f69751ba6d29f46aea
Mattia <mattia.tummolilllo@gmail.com>
2020-10-16 11:14:06 +0200

    Primi 3 esempi in JS (HELLO WORLD)

---
+++ LC JAVASCRIPT/js/script.js
@@ -0,0 +1,9 @@
+//Esempio uno
+document.writeln("Hello World!");
+
+//Esempio document
+alert("Hello World! in alert...");
+
+//Esempio tre
+//Inseriamo il testo nell'h1 vuoto da js
+document.getElementById("titolo").innerHTML = "sono un titolo inserito dinamicamente da js, non è fantastico?";
