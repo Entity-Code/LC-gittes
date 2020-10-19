7f3f5fe5ce6571f8ff6685f539e7dc878aacf478
Mattia <mattia.tummolilllo@gmail.com>
2020-10-19 11:07:33 +0200

    Utilizzo input prompt (Domandare il nome e salutare l'utente)

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,29 +1,6 @@
-// dichiarazione variabili globali
-// var variabileUno;
-// var variabileDue;
-
-// oppure
-// var variabileUno, variabileDue;
-
-
-// Stampa del primo valore (undefined)
-var miaStringaUno, miaStringaDue, miaStringaTotale;
-var numeroUno, numeroDue, numeroTot
-
-//Lavoriamo con le stringhe
-miaStringaUno = "CIAO";
-miaStringaDue = 10;
-
-console.log(miaStringaUno, miaStringaDue);
-
-miaStringaTotale = miaStringaUno + miaStringaDue;
-
-document.getElementById("titolo").innerHTML = miaStringaTotale;
-
-//Lavoriamo con i numeri
-
-numeroUno = 10;
-numeroDue = 7;
-numeroTot = numeroUno + numeroDue;
-
-document.getElementById("stampanumero").innerHTML = numeroTot;
+// chiedo all'utente il suo nome
+var text = prompt("inserisci il tuo nome");
+var saluto ="Benvenuto ";
+console.log(text);
+// saluto l'utente
+document.getElementById("titolo").innerHTML = saluto + text;
