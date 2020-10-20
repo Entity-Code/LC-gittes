d027b12d44a50db9149d8fc0ede83204128c38ce
Mattia <mattia.tummolilllo@gmail.com>
2020-10-20 11:36:58 +0200

    Date

--- LC Conditions/js/script.js
+++ LC Conditions/js/script.js
@@ -1,33 +1,3 @@
-// messaggio
-var msg;
-//input: richiesta inserimento parola1
-var parola1 = prompt("Inserisci la prima parola");
+var d = new Date();
 
-//input: richiesta inserimento parola2
-var parola2 = prompt("Inserisci la seconda parola");
-
-console.log(parola1.length, parola2.length);
-
-
-
-//verifico quale parola sia più lunga
-   //se parola1 è più lunga creo messaggio corrispondente
-   if (parola1.length > parola2.length) {
-      // creo messaggio corrispondente
-      msg = "parola1 è più lunga";
-      console.log("parola1 è più lunga");
-   } else if (parola2.length > parola1.length) { //se parole2 è più lunga
-      // creo messaggio corrispondente
-      msg = "parola2 è più lunga";
-      console.log("parola2 è più lunga");
-   } else { //altrimenti se sono uguali
-      // creo messaggio corrispondente
-      msg = "parola1 è più lunga";
-      console.log("le parole sono lunghe uguali");
-   }
-   // se parola2 è più lunga creo messaggio corrispondente
-
-   // altrimenti se sono uguali creo messaggio corrispondente
-
-//output: per dire quale parola è più lunga
-document.getElementById("titolo").innerHTML = msg;
+console.log(d.getMonth(), d.getHours(), d.getMinutes());
