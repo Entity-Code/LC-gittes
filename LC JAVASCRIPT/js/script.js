57976ceef8e93e7a8ea1df74e68609f3af2e0792
Mattia <mattia.tummolilllo@gmail.com>
2020-10-21 12:42:33 +0200

    elemento.value input funzionamento (in sostituzione al prompt)

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,22 +1,12 @@
-// iL SOFTWARE DEVE CHIEDERE ALL'UTENTE IL SUO NOME E IL SESSO CON DUE PROMPT DIVERSI.
-// SULLA PAGINA HTML DEVE APPARIRE "CIAO <NOME>",
-// IL COLORE DEL NOME DEVE ESSERE AZZURRO O ROSA A SECONDA DEL SESSO INSERITO
+var titolo = document.getElementById("titolo");
+var campoInput = document.getElementById("campoTesto");
 
-// selezione elemento
-var el = document.getElementById("titolo");
+console.log(campoInput.value);
 
-// chiedo nome nomeutente
-var nome = prompt("inserisci il nome");
+var bottone = document.getElementById("mybutton");
 
-// chiedo sesso utente
-var sesso = prompt("inserisci il sesso M o F");
-
-// output nome in pagina
-document.getElementById("titolo").innerHTML = "Ciao" + nome;
-
-// gestire colore output
-if (sesso === "M") {
-   el.className = "blu"; //classe "blu" presa dal css
-} else {
-   el.className = "rosa"; //classe "rosa" presa dal css
-}
+bottone.addEventListener("click",
+	function() {
+	  titolo.innerHTML = campoInput.value;
+	}
+);
