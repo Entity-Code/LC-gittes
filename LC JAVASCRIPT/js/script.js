990b668efa80a033cfc39e3e44f7212121ef140e
Mattia <mattia.tummolilllo@gmail.com>
2020-10-21 11:45:02 +0200

    Aggiunta classe creata in css da js per cambiare il colore dell'input

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,7 +1,8 @@
 var elemento = document.getElementById("nomeutente");
-
 var nome = prompt("inserisci il tuo nome");
 
 elemento.innerHTML = nome;
 
-elemento.style.color = "red";
+// elemento.style.color = "red";
+
+elemento.className = elemento.classList + "redText";
