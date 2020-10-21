b258d7de5d9459a1310036989891db9547a390cc
Mattia <mattia.tummolilllo@gmail.com>
2020-10-21 11:37:53 +0200

    Aggiunta colore del testo di input con .style.color

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,11 +1,7 @@
-// chiedere all'utente quanti anni
-var eta = parseInt(prompt("Scrivi quanti anni hai"));
-var mail = prompt ("Scrivi la tua mail");
-var annoCorrente = 2020;
-var annoNascita;
-console.log(eta);
+var elemento = document.getElementById("nomeutente");
 
-annoNascita = annoCorrente - eta;
-console.log("l'output sarà: ", annoNascita);
-// dire all'utente in che anno è nato
-document.getElementById("titolo").innerHTML = "Hai " + eta + " anni, quindi " + "sei nato nel " + annoNascita + "." + " Ti invieremo questo dato inaspettato alla mail: " + mail;
+var nome = prompt("inserisci il tuo nome");
+
+elemento.innerHTML = nome;
+
+elemento.style.color = "red";
