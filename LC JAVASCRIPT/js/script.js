0adc721bc412e2dacd700b2b791a48aa7c080323
Mattia <mattia.tummolilllo@gmail.com>
2020-10-22 11:49:12 +0200

    Stampa tutti i numeri da 1000 a 0 (contrario) CICLO FOR

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,10 +1,16 @@
 // Stampa tutti i numeri da 0 a 1000
 
-for (var i = 0; i <= 1000; i++) {
-	console.log("il ciclo for è al numero di iterazione: ",i);
-}
+// for (var i = 0; i <= 1000; i++) {
+// 	console.log("il ciclo for è al numero di iterazione: ",i);
+// }
+//
+// console.log("valore finale di i: ", i);
 
-console.log("valore finale di i: ", i);
+// Stampa tutti i numeri da 1000 a 0
+
+for (var j = 1000; j >= 0; j--) {
+	console.log("il ciclo for è al numero di iterazione: ", j);
+}
 
 
 
