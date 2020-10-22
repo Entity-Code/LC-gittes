968fab644d494ead68857ad40d3a12adb0700863
Mattia <mattia.tummolilllo@gmail.com>
2020-10-22 11:05:50 +0200

    Uso array con presa dell'indice e push

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,6 +1,27 @@
-// Chiedi un numero all'utente e stampa in console il numero successivo.
+//Crea un Array contenente 3 nomi.
+// Chiedi un numero all'utente da 0 a 2 all'utente e stampa l'elemento dell'array corrispondente
 
-var numero =  parseInt(prompt("numero grazie"));
+// Dichiarazione array
+var mioArray = ["nome0", "nome1", "nome2"];
 
-console.log(numero++); //numero = numero + 1;
-console.log(numero);
+// Richiamo di un elemento dell'array
+var indice = parseInt(prompt("dammi un indice tra 0 e 2"));
+
+
+// accedere all'indice dell'array
+var arrayItem = mioArray[indice];
+
+console.log("array completo: ", mioArray);
+console.log("item selezionato: ", arrayItem);
+
+
+//la proprietà .length ci da informazioni sulla lunghezza dell'array (3 elementi ora)
+console.log("lunghezza array iniziale: ", mioArray.length);
+
+//aggiunta dell'elemento "nome3"
+mioArray.push("nome3");
+
+
+console.log("lunghezza array dopo 1 push: ", mioArray.length);
+
+console.log("array completo dopo 1 push: ", mioArray);
