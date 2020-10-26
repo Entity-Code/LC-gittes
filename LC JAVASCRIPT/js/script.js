69f3b7522dd700144ef330738007fac0766ae7e6
Mattia <mattia.tummolilllo@gmail.com>
2020-10-26 12:12:57 +0100

    Es. parola più lugna

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,11 +1,15 @@
-//numero 1
-var n1 = parseInt(prompt("Inserisci il primo numero"));
-//numero 2
-var n2 = parseInt(prompt("Inserisci il secondo numero"));
+//parola 1
+var p1 = prompt("Inserisci la prima parola");
+//parola 2
+var p2 = prompt("Inserisci la seconda parola");
 
-// output
-if (n1 > n2) {
-	document.write("<h3>Il numero più grande è : </h3>" + n1);
-} else {
-	document.write("<h3>Il numero più grande è : </h3>" + n2);
-}
+// OUTPUT
+	// p1>p2
+	if (p1.length > p2.length) {
+		document.write("<h3>La parola più corta è : </h3>" + p2);
+		document.write("<h3>La parola più lunga è : </h3>" + p1);
+	// p1<p2
+	} else {
+		document.write("<h3>La parola più corta è : </h3>" + p1);
+		document.write("<h3>La parola più lunga è : </h3>" + p2);
+	}
