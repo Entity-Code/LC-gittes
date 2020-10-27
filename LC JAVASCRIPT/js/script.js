e902e7530ede3d653fa01450dea086b9a3bc87d2
Mattia <mattia.tummolilllo@gmail.com>
2020-10-27 12:13:04 +0100

    Verifica tramite una funzione se il numero inserito è pari o dispari.

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,17 +1,32 @@
-// Genero 10 numeri random utilizzando una funzione
+// // Genero 10 numeri random utilizzando una funzione
+//
+// //max = è l'argomento che creiamo noi che corrisponde al range massimo nel quale verranno creati i numeri random (da 0 a 50)
+// function generamiUnNumeroRandom(max) {
+//    if (max) { //se max è stato inserito, allora ritornami il valore random
+//    return Math.floor(Math.random() * max) + 1;
+//    } else { //se non è stato inserito, allora stampa questo messaggio
+//       return "non hai inserito il valore dell'argomento";
+//    }
+// }
+//
+//
+// //generami 10 numeri, ognuno random con un range da 0 a max (che inseriamo noi al richiamo della funzione che mi genera un random number da 0 a (50))
+// for (var i = 0; i < 10; i++) {
+//    var numero = generamiUnNumeroRandom();
+//    console.log(numero);
+// }
 
-//max = è l'argomento che creiamo noi che corrisponde al range massimo nel quale verranno creati i numeri random (da 0 a 50)
-function generamiUnNumeroRandom(max) {
-   if (max) { //se max è stato inserito, allora ritornami il valore random
-   return Math.floor(Math.random() * max) + 1;
-   } else { //se non è stato inserito, allora stampa questo messaggio
-      return "non hai inserito il valore dell'argomento";
+// Verifico attraverso una funzione se il numero passato dell'utente è pari o dispari
+
+var inputUser = parseInt(prompt("inserisci un numero"));
+
+console.log(verificaPariODispari(inputUser));
+
+//definisco la funzione
+function verificaPariODispari(num) {
+   if (num % 2 === 0) {
+      return "pari";
+   } else {
+      return "dispari";
    }
-}
-
-
-//generami 10 numeri, ognuno random con un range da 0 a max (che inseriamo noi al richiamo della funzione che mi genera un random number da 0 a (50))
-for (var i = 0; i < 10; i++) {
-   var numero = generamiUnNumeroRandom();
-   console.log(numero);
 }
