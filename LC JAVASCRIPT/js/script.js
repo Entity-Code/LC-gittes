447ff8af86cf0b18ee945b767133d1d3c8f8368a
Mattia <mattia.tummolilllo@gmail.com>
2020-11-02 10:46:40 +0100

    LC selettori jQuery

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,30 +1,21 @@
-// Generatore di “nomi cognomi” casuali: prendendo a caso un nome e un cognome da una lista di nomi e da una lista di cognomi, Gatsby vuole generare una falsa lista di invitati.
-
-//creo array nomi
-var nomi = ["Mattia ","Mario ","Martina ","Sofia ","Luigi ","Francesco "];
-console.log("Lista nomi: " + nomi);
-//creo array cognomi
-var cognomi = ["Barretta ","Tummolillo ","Rossi ","Ferrari ","Russo ","Bianchi "];
-console.log("Lista cognomi: " + cognomi);
+// Data una struttura html selezionare gli elementi indicati e applicare loro una classe con la funzione .addClass()
 
 
-var casualNomiCognomi = [];
-//casual
-i = 0;
-while (i < nomi.length) {
-   var casualNome = randomNum(nomi.length);
-   var casualCognome = randomNum(cognomi.length);
-   casualNomiCognomi.push(nomi[casualNome] + cognomi[casualCognome]);
-   i++;
-}
-console.log(casualNomiCognomi);
+
+// // seleziono i tag h1 e gli aggiungo la classe
+// $("h1").addClass("classe-aggiunta");
+
+// salvo la selezione in una var
+var titolo = $("h1");
+// aggiungo il metodo addClass() alla var
+titolo.addClass("classe-aggiunta");
 
 
-// random Num
-function randomNum(max) {
-   if (max) { //se max è stato inserito, allora ritornami il valore random
-   return Math.floor(Math.random() * max);
-} else { //sennò 0
-      return 0;
-   }
-}
+
+// $("p").addClass("classe-aggiunta");
+
+$("p.paragrafo-sel").addClass("classe-aggiunta");
+
+$("#divvone").addClass("classe-aggiunta");
+
+titolo.removeClass("classe-aggiunta");
