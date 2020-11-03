8a1ba0da35923bb7cf36cc4116be9ec1f93d0cd1
Mattia <mattia.tummolilllo@gmail.com>
2020-11-03 10:47:03 +0100

    USO THIS CON "CHILDREN" E "FIND" (selezione elementi specifici)

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
 
+   //mettiamo i div nascosti
+   $(".faq p").hide();
+
+   //selezioniamo il padre, e con this.children("p") selezioniamo l'elemento suo figlio diretto a cui vogliamo applciare . slideToggle
+   $(".faq").click(function () {
+      // figlio diretto
+      $(this).children("p").slideToggle("fast");
+      // // discendente generale
+      // $(this).find("p").slideToggle("fast");
+   });
 
 
 
 
-   $(".buttons").click(function () {
-      $(this).toggleClass("qualsiasi");
-      // $(".buttons").hide();
-   })
+
 });
