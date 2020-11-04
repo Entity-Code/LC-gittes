4713efe0a8a91982f004c6c5b56cc65e1760c289
Mattia <mattia.tummolilllo@gmail.com>
2020-11-04 12:52:03 +0100

    intro dropdown singolo menù, allineato (JS/JQ)

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,29 +1,11 @@
-// Creare 10 quadrati vuoti con jQuery
-// Associare una classe red ai quadrati in posizione pari e una classe green ai quadrati in posizione dispari
+//dropdown google simile
 
 $(document).ready(function () {
 
-   // aggiungo 10 volte dei div .square
-   for (var i = 0; i < 10; i++) {
-      var square = '<div class="square"></div>';
-      //  aggiungo 10 square nel container .squares
-      $(".squares").append(square);
-   }
 
-   $(".square").each(function (index, element) {
-      console.log(index, element);
-      if (index % 2 === 0) { // indice pari
-         $(element).addClass("red");
-      } else {
-         $(element).addClass("green");
-      }
+   $("li.dropitem").click(function() {
+      $(".dropdown").toggleClass("dbloc");
    })
-
-   // metodo jquery instant
-   // $(".square").odd().addClass("red");
-   // $(".square").even().addClass("green");
-
-
 
 
 
