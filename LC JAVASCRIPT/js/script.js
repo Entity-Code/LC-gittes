d67e295a23c00644184fd46288f02678ed141234
Mattia <mattia.tummolilllo@gmail.com>
2020-11-05 12:30:58 +0100

    setInterval (refactoring)

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,13 +1,16 @@
 //Simulare un countdown di 10 secondi che alla fine dice Buon Anno!
 
-var mioInterval = setInterval(countdown, 500);
+//ogni secondo esegui la funzione countdown
+var mioInterval = setInterval(countdown, 1000);
 
+//il count parte da 10
 var count = 10;
 function countdown() {
+   // decrementa di 1 e stampa il decremento fino a quando count > 0
    if (count > 0) {
       console.log(count);
       count--;
-   } else {
+   } else { //se count non è più > 0, allora stampa Buon Anno, e pulisci il setInterval
       console.log("Buon anno!!");
       clearInterval(mioInterval);
    }
