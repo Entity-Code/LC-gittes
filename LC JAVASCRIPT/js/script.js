635ac4fc1a172ccdb0378cbcc92d0ed465b30187
Mattia <mattia.tummolilllo@gmail.com>
2020-11-09 10:34:42 +0100

    OBJECT: creazione e richiamo chiave

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,17 +1,11 @@
-//Simulare un countdown di 10 secondi che alla fine dice Buon Anno!
+// creo un oggetto
 
-//ogni secondo esegui la funzione countdown
-var mioInterval = setInterval(countdown, 1000);
+var palla = {
+  "colore": ['red', 'blue'], //il colore è un array
+  'tipo': 'pallina da golf',
+};
 
-//il count parte da 10
-var count = 10;
-function countdown() {
-   // decrementa di 1 e stampa il decremento fino a quando count > 0
-   if (count > 0) {
-      console.log(count);
-      count--;
-   } else { //se count non è più > 0, allora stampa Buon Anno, e pulisci il setInterval
-      console.log("Buon anno!!");
-      clearInterval(mioInterval);
-   }
-}
+// se volessi farmi ritornare il tipo, scrivo :
+console.log(palla.tipo);
+// se volessi farmi ritornare il colore, scrivo :
+console.log(palla.colore);
