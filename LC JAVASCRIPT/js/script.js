6436e0a6bafaeda5bbb676fb82fd4e9522ff366d
Mattia <mattia.tummolilllo@gmail.com>
2020-11-12 10:00:54 +0100

    MAP

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,8 +1,9 @@
-const students = ['Paolo', 'Giulia', 'Marco'];
-//arrow function con 3 argomenti
-students.forEach((element, index, array) => {
-  console.log(element, index);
-});
-// Paolo 0
-// Giuglia 1
-// Marco 2
+// dichiaro un array
+const array1 = [1, 4, 9, 16];
+
+// passo una function arrow che moltiplica x(ogni valore di array1) per due
+//"mappo" ogni elemento, -> si crea un nuovo array map1 che sarà una copia di array1 con le modifiche nella arrow, ovvero i valori moltiplicati per due
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
