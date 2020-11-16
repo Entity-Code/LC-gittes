5ec8e2b2f10ca68e78691309b55dc51f52f34707
Mattia <mattia.tummolilllo@gmail.com>
2020-11-16 11:01:12 +0100

    v-bind VUE

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,3 +1,5 @@
+/*
+// V-model
 // Utilizzare due input, in cui inseriremo Nome e Cognome.
 // In un paragrafo utilizzare Nome e Cognome per salutare l'utente
 
@@ -5,9 +7,24 @@
 //istanza vue
 
 var app = new Vue({
+   // elemento da selezionare
    el: "#app",
+   // dati
    data: {
       nome: "nome",
       cognome: "cognome",
+   }
+});
+*/
+
+//V-bind
+var app = new Vue({
+   // elemento da selezionare
+   el: "#app",
+   // dati
+   data: {
+      nome: "nome",
+      cognome: "cognome",
+      mieClassi: "classe1 classe2"
    }
 });
