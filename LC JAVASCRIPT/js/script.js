dc68be74b26489d9e95e4b94703a307d00b33051
Entity-Code <mattia.tummolilllo@gmail.com>
2020-12-01 10:03:09 +0100

    Correzione v-for nell'ul

--- LC JAVASCRIPT/js/script.js
+++ LC JAVASCRIPT/js/script.js
@@ -1,90 +1,21 @@
-/*
-// V-model
-// Utilizzare due input, in cui inseriremo Nome e Cognome.
-// In un paragrafo utilizzare Nome e Cognome per salutare l'utente
+// //prendimi in questo percorso dei dati
+// axios.get("https://flynn.boolean.careers/exercises/api/random/int")
+// .then(function(risposta) { //salvameli in "risposta"
+//    //salva l'oggetto ritornato in una costante (result)
+// 	const result = risposta.data; // interrogalo con .data
+//    console.log(result); //mostramelo
+// });
 
-
-//istanza vue
 
 var app = new Vue({
-   // elemento da selezionare
    el: "#app",
-   // dati
    data: {
-      nome: "nome",
-      cognome: "cognome",
-   }
-});
-*/
-
-// //V-bind
-// var app = new Vue({
-//    // elemento da selezionare
-//    el: "#app",
-//    // dati
-//    data: {
-//       nome: "nome",
-//       cognome: "cognome",
-//       mieClassi: "classe1 classe2"
-//    }
-// });
-
-
-// var app = new Vue({
-//    // elemento da selezionare
-//    el: "#app",
-//    // dati
-//    data: {
-//       nome: "nome",
-//       cognome: "cognome",
-//       mieClassi: "classe2"
-//    },
-//    methods: {
-//       changeTitle: function() {
-//          //puro javascript
-//
-//          //this si riferisce al dato "mieClassi", sostituendo il suo valore
-//          if (this.mieClassi === "classe2") {
-//             this.mieClassi = "classe1";
-//          } else {
-//             this.mieClassi = "classe2";
-//          }
-//          console.log(this.cognome);7
-//
-//       }
-//    }
-// });
-
-// var app = new Vue({
-//    // elemento da selezionare
-//    el: "#app",
-//    // dati
-//    data: {
-//       titleShow: false,
-//       names: ['Michele', 'Nicola', 'Mario']
-//    },
-//    methods: {
-//       toggleVisible: function () {
-//          if (this.titleShow === true) {
-//             this.titleShow = false
-//          } else {
-//             this.titleShow = true
-//          }
-//       }
-//    }
-// });
-
-// todo list
-var container = new Vue({
-   el: "#app",
-
-   data: {
-      items: ["cucinare", "stirare", "lavorare", "dormire"],
+      item: "",
    },
-
-   methods: {
-      addItem() {
-         console.log("newItem");
-      }
+   mounted: function () {
+      axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((risposta) => {
+         console.log(risposta.data);
+      });
    }
 });
